a7c05b9942cbed83b5d6d894f711bdedd9956f18
samane-92 <131759514+samane-92@users.noreply.github.com>
2023-06-25 11:15:27 +0430

    Added bootstrap

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -2,16 +2,23 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div></div>
-      <footer>
-        <a href="https://github.com/samane-92/weather-app" target="_blank">Open-sourced app on GitHub</a>{" "}
-        coded by
-        <a href="https://github.com/samane-92" target="_blank"> Samaneh R.</a>
-      </footer>
+      <div className="App">
+        <div className="container">
+          <footer>
+            <a href="https://github.com/samane-92/weather-app" target="_blank">
+              Open-sourced app on GitHub
+            </a>{" "}
+            coded by
+            <a href="https://github.com/samane-92" target="_blank">
+              {" "}
+              Samaneh R.
+            </a>
+          </footer>
+        </div>
+      </div>
     </>
   );
 }
