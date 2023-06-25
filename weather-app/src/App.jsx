ebf65be11847a7a35fe0c0296ce4fb99fa18f520
samane-92 <131759514+samane-92@users.noreply.github.com>
2023-06-25 09:49:47 +0430

    Removed unnecessary files

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1>HELLLO</h1>
+        <h2>samaneh</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
